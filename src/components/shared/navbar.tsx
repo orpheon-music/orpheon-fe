477edb566794c1bc8466ac054986a6ffb46e548d
kenziehh <ghaisani.nurani@gmail.com>
2025-07-10 19:02:06 +0700

    feat: refactor Navbar component to wrap logo in Link and adjust button layout

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -2,14 +2,16 @@ import Image from 'next/image'
 import React from 'react'
 import { Button } from '../ui/button'
 import { ArrowUpRight } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <nav className="fixed bottom-10 left-0 right-0 z-[9999] backdrop-blur-md">
             <div className="container">
                 <div className="flex justify-between items-center">
-                    <Image alt="Orpheon Logo" src={"/assets/images/logo_nav.png"} width={200} height={88} className="" />
-
+                    <Link href={"/"} className='hidden md:block'>
+                        <Image alt="Orpheon Logo" src={"/assets/images/logo_nav.png"} width={200} height={88} className="" />
+                    </Link>
                     <div className="hidden md:flex items-center space-x-32 bg-[#010101]/15 rounded-[72px] px-12 py-6">
                         <div className='md:flex items-center space-x-8'>
                             <a href="#" className="text-white hover:text-pink-400 transition-colors duration-200 font-medium">
@@ -25,12 +27,17 @@ export default function Navbar() {
                                 Pricing
                             </a>
                         </div>
-                        <Button className="bg-white text-black hover:bg-gray-200 transition-all duration-200 font-semibold px-6 py-2 rounded-full">
-                            Get Started
-                            <ArrowUpRight className="ml-2 h-4 w-4" />
-                        </Button>
+                        <div className='flex items-center gap-4'>
+                            <span>
+                                Get Started
+                            </span>
+                            <Button className="bg-white w-14 h-14 flex justify-center items-center text-black hover:bg-gray-200 transition-all duration-200 font-semibold px-6 py-2 rounded-full">
+                                <ArrowUpRight className="h-4 w-4" />
+                            </Button>
+                        </div>
+
                     </div>
-                    <div className='block'/>
+                    <div className='hidden xl:block' />
 
                 </div>
 
