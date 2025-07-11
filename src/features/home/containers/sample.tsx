b523a6cfd01f6041e7a1293a3efe8e1ec6ae8200
kenziehh <ghaisani.nurani@gmail.com>
2025-07-12 06:42:11 +0700

    feat: add Sample component with audio transformation features and images

---
+++ src/features/home/containers/sample.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { useState } from 'react'
+import Disk from "@/assets/images/home/sample-disk.png"
+import Ellipse from "@/assets/images/home/ellipse-sample.png"
+
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
+
+export default function Sample() {
+  const [selected, setSelected] = useState<"AI-Assisted" | "Original">("AI-Assisted")
+
+  return (
+    <section className='mt-20 md:mt-52 bg-black py-20'>
+      <div className='flex justify-center items-center flex-col gap-8 z-30'>
+        <div className='flex gap-2 items-center py-2 px-4 border border-white'>
+          <div className='w-8 h-8 bg-white rounded-full' />
+          <span className='text-sm md:text-base text-[F8F8FF]'>Sample</span>
+        </div>
+        <div className='flex flex-col gap-4 items-center'>
+          <h1 className='text-[30px] md:text-[54px] text-white text-center'>Music Sample</h1>
+          <p className='text-[#A1A4A7] text-lg md:text-2xl max-w-[640px] text-center'> Listen to how our innovative technology transforms raw audio into polished tracks, showcasing clarity and depth that elevates your sound to professional standards.</p>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="flex rounded-full border border-white/30 bg-white/5 p-1 backdrop-blur-sm">
+            {["AI-Assisted", "Original"].map((label) => (
+              <button
+                key={label}
+                onClick={() => setSelected(label as "AI-Assisted" | "Original")}
+                className={cn(
+                  "px-6 py-2 text-sm font-medium rounded-full transition-all duration-300",
+                  selected === label
+                    ? "bg-white/10 text-white shadow-inner"
+                    : "text-white/80"
+                )}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-center items-center relative'>
+        <Image
+          src={Disk}
+          alt="Sample Disk"
+          className='z-30'
+        />
+        <Image
+          src={Ellipse}
+          alt="Sample Ellipse"
+          className='absolute z-10'
+        />
+
+      </div>
+
+    </section>
+  )
+}
