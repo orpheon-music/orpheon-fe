8be1170a4c877753c15e97cf92502910782ff30c
kenziehh <ghaisani.nurani@gmail.com>
2025-07-12 07:06:55 +0700

    fix: fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
-import { ArrowRight } from "lucide-react"
 import Hero from "@/features/home/containers/hero"
 import HowItWorks from "@/features/home/containers/how-it-works"
 import Pricing from "@/features/home/containers/pricing"
